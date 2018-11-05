0ce897b77d52e306e74ab315268faba97fefa0c8
Emrys West <westemrys@gmail.com>
2018-11-05 08:47:07 -0700

    todo tutorial start

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,23 +1,27 @@
 import React from 'react';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <div class="app-container">
-          <div class="players-container">
-            <div class="player">Player 1</div>
-            <div class="player">Player 2</div>
-            <div class="player">Player 3</div>
-            <div class="player">Player 4</div>
-          </div>
-          <div class="bag-container">
-            <div class="bag-item">item 1</div>
-            <div class="bag-item">item 2</div>
-            <div class="bag-item">item 3</div>
-          </div>
-        </div>
-      </div>
-    );
-  }
-}
+
+
+
+
+// export default class App extends React.Component {
+//  render() {
+//    return (
+//      <div class="container">
+//        <div class="app-container">
+//          <div class="players-container">
+//            <div class="player">Player 1</div>
+//           <div class="player">Player 2</div>
+//            <div class="player">Player 3</div>
+//            <div class="player">Player 4</div>
+//          </div>
+//          <div class="bag-container">
+//            <div class="bag-item">item 1</div>
+//            <div class="bag-item">item 2</div>
+//            <div class="bag-item">item 3</div>
+//          </div>
+//        </div>
+//      </div>
+//    );
+//  }
+//}
